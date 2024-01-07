5fd515010c50ef94e6f4494457a5de4282208c83
Salvatore Isaja <salvatore.isaja@ynap.com>
2024-01-07 13:42:54 +0100

    Added helpers for read-only collections.

---
+++ DiscriminatedOnions/ReadOnlyDictionary.cs
@@ -0,0 +1,48 @@
+﻿/*
+DiscriminatedOnions - A stinky but tasty hack to emulate F#-like discriminated unions in C#
+
+Copyright 2022-2024 Salvatore ISAJA. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiscriminatedOnions;
+
+public static class ReadOnlyDictionary
+{
+    private static class EmptyDictionary<TKey, TValue> where TKey : notnull
+    {
+        public static readonly IReadOnlyDictionary<TKey, TValue> Value = new Dictionary<TKey, TValue>();
+    }
+
+    public static IReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<T, TKey, TValue>(this IEnumerable<T> source, Func<T, TKey> keySelector, Func<T, TValue> valueSelector) where TKey : notnull =>
+        source.ToDictionary(keySelector, valueSelector);
+
+    public static IReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector) where TKey : notnull =>
+        source.ToDictionary(keySelector);
+
+    public static IReadOnlyDictionary<TKey, TValue> Empty<TKey, TValue>() where TKey : notnull =>
+        EmptyDictionary<TKey, TValue>.Value;
+}
---
+++ DiscriminatedOnions/ReadOnlySet.cs
@@ -0,0 +1,44 @@
+﻿/*
+DiscriminatedOnions - A stinky but tasty hack to emulate F#-like discriminated unions in C#
+
+Copyright 2022-2024 Salvatore ISAJA. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiscriminatedOnions;
+
+public static class ReadOnlySet
+{
+    private static class EmptySet<T>
+    {
+        public static readonly IReadOnlySet<T> Value = new HashSet<T>();
+    } 
+
+    public static IReadOnlySet<T> ToReadOnlySet<T>(this IEnumerable<T> source) =>
+        source.ToHashSet();
+
+    public static IReadOnlyCollection<T> Empty<T>() =>
+        EmptySet<T>.Value;
+}
