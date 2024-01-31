17581ddd67e01f789599a7c04d56d0ba3c1efbb8
mykyta.kysil <mykyta.kysil@charge-amps.com>
2024-01-31 12:27:25 +0200

    fix: whitespace

--- DiscriminatedOnions.Tests/ResultTest.cs
+++ DiscriminatedOnions.Tests/ResultTest.cs
@@ -191,4 +191,20 @@ public static class ResultTest
                 onError: e => (e, DummyResultValue),
                 onOk: v => (DummyErrorValue, v))))
         .Should().Be((43, DummyResultValue));
+    
+    [Test]
+    public static void Error_ImplicitConversion()
+    {
+        Result<string, int> ReturnErrorImplicit() => DummyErrorValue;
+    
+        Result.Error<string, int>(DummyErrorValue).ErrorValue.Should().Be(ReturnErrorImplicit().ErrorValue);
+    }
+
+    [Test]
+    public static void Ok_ImplicitConversion()
+    {
+        Result.Ok<string, int>(DummyResultValue).ResultValue.Should().Be(OkImplicit().ResultValue);
+
+        Result<string, int> OkImplicit() => DummyResultValue;
+    }
 }
